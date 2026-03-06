2a130fe900cdfc2531b0df35ee89d442a18e02b0
vilnisjatnieks <vjatnieks@loyola.edu>
2026-03-06 14:49:59 -0500

    feat: update hero title font and add animated gradient hover effect

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,14 +1,15 @@
 
+import resumeFile from '../assets/Kaija Lazda Resume.docx';
 
 const Navigation = () => {
     return (
         <nav className="main-nav">
             <ul>
-                <li><a href="#" className="nav-link">Home</a></li>
                 <li><a href="https://www.flipsnack.com/895C9D5EFB5/design-is-final" target="_blank" rel="noopener noreferrer" className="nav-link">About Me</a></li>
                 <li><a href="#portfolio" className="nav-link">Portfolio</a></li>
                 <li><a href="mailto:Kaija.Lazda@colorado.edu" className="nav-link">Contact</a></li>
-                <li><a href="#" className="nav-link">Experience</a></li>
+                <li><a href="#experience" className="nav-link">Experience</a></li>
+                <li><a href={resumeFile} target="_blank" rel="noopener noreferrer" className="nav-link">Resume</a></li>
             </ul>
         </nav>
     );
